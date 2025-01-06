e69464c3af5d2ed76f1cdbbd896fe5707d63d0f3
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2025-01-06 14:34:10 -0500

    style: add styling to comment

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,11 +1,25 @@
+import styles from "./Comment.module.css";
 /* eslint-disable react/prop-types */
 export default function Comment({author, content, timestamp}) {
+  function formatDate(timestamp) {
+    const date = new Date(timestamp)
+    return date.toLocaleDateString('en-US', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+    })
+  }
+
   return (
-    <div>
-      <h1>{author}</h1>
+    <div className={styles.commentDiv}>
+      <div className={styles.commentHeader}>
+        <p className={styles.commentAuthor}>{author}</p>
+        <p className={styles.commentDate}>{formatDate(timestamp)}</p>
+      </div>
       <p>{content}</p>
-      <p>{timestamp}</p>
-      <p>{author}</p>
     </div>
   )
 }
--- src/pages/SingleBlogPage/SingleBlogPage.jsx
+++ src/pages/SingleBlogPage/SingleBlogPage.jsx
@@ -11,12 +11,19 @@ export default function SingleBlogPage() {
   const [error, setError] = useState(null);
   const auth = useAuth();
 
+  function formatDate(timestamp) {
+    const date = new Date(timestamp)
+    return date.toLocaleDateString('en-US', {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    })
+  }
+
   useEffect(() => {
     fetch(`http://localhost:8080/posts/${postId}`, {
       method: "GET",
-      headers: {
-        // 'Authorization': token
-      }
     })
     .then((response) => {
       if (response.status >= 400) {
@@ -29,7 +36,7 @@ export default function SingleBlogPage() {
       setPost(data.post);
     })
     .catch((error) => setError(error))
-  }, [post])
+  }, [])
 
 
   return (
@@ -39,9 +46,9 @@ export default function SingleBlogPage() {
         <div>
           <h1>SINGLE BLOG PAGE</h1>
           <h2>{post.title}</h2>
-          <p>{post.timestamp}</p>
+          <p>Written By: {post.author.username}</p>
+          <p>{formatDate(post.timestamp)}</p>
           <p>{post.content}</p>
-          <p>{post.author.username}</p>
           <CommentForm />
             {post.comments.map((comment) => {
               return (
