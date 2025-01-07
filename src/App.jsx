3a3bc9ae0aeb215f9b598d376a482992c21a1ba0
cheet <29075792+ImmaCheetah@users.noreply.github.com>
2025-01-07 12:08:24 -0500

    style: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import PropTypes from 'prop-types';
 import './App.css'
 import Navbar from "./components/Navbar/Navbar";
+import Footer from './components/Footer/Footer';
 import AuthProvider from './components/AuthProvider/AuthProvider';
 
 const RenderName = (props) => {
@@ -43,6 +44,7 @@ const App = () => {
       <main>
           <Outlet />
       </main>
+      <Footer />
     </AuthProvider>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>Made by <a href="https://github.com/ImmaCheetah" target="blank">Cheet</a></p>
+    </footer>
+  )
+}
